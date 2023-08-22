55757d0af9d66d26f439e398b7ef653ada0a8bcd
Itay <Itain2010@gmail.com>
2023-08-22 16:46:48 +0300

    soldier body index function

--- classes/Soldier.py
+++ classes/Soldier.py
@@ -75,4 +75,21 @@ class Soldier:
             x_square = self.x // SQUARE_SIZE
             indexes.append([y_square + SOLDIER_HEIGHT_SQUARES - 1, x_square+col])
         return indexes
+    def get_body_index(self):
+        """Returns the index of the soldier body"""
+        indexes=[]
+        for row in range(SOLDIER_HEIGHT_SQUARES):
+            for col in range(SOLDIER_WIDTH_SQUARES):
+                y_square = self.y // SQUARE_SIZE
+                x_square = self.x // SQUARE_SIZE
+                indexes.append([y_square+row, x_square+col])
+        # Remove the legs indexes
+        leg_indexes = self.get_legs_index()
+        for leg_index in leg_indexes:
+            indexes.remove(leg_index)
+        return indexes
+
+
+
+
 
