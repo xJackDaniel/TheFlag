af19d326c11bb6e952eb2d986671d75bd6df2d95
Student <denikorenfeld@gmail.com>
2023-08-23 09:21:42 +0300

    check lose - finished

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import screens
 game_field = GameField()
 board = game_field.get_board()
 # Create screen
-screenObj = Screen(game_field)
+screenObj = Screen()
 screen = screenObj.get_screen()
 # Create soldier
-soldier = Soldier(screenObj, game_field)
+soldier = Soldier(game_field)
 
 
 def main():
@@ -34,7 +34,7 @@ def main():
                 if key_input[pygame.K_RETURN]:
                     mine_screen = True
                     # Display the mine screen for x seconds
-                    screens.display_mine_screen(soldier)
+                    screens.display_mine_screen(soldier, game_field)
                     # Update the display
                     pygame.display.update()
                     # Delay the mine screen
@@ -42,26 +42,27 @@ def main():
                     # Return to regular screen
                     mine_screen = False
                 elif key_input[pygame.K_LEFT]:
-                    soldier.move_x(right=False)
+                    soldier.move_x(right=False, game_field=game_field)
                 elif key_input[pygame.K_RIGHT]:
-                    soldier.move_x(right=True)
+                    soldier.move_x(right=True, game_field=game_field)
                 elif key_input[pygame.K_DOWN]:
-                    soldier.move_y(up=False)
+                    soldier.move_y(up=False, game_field=game_field)
                 elif key_input[pygame.K_UP]:
-                    soldier.move_y(up=True)
+                    soldier.move_y(up=True, game_field=game_field)
 
         # Display the regular screen
+        soldier.change_soldier_image(SOLDIER_IMG_PATH)
         screens.display_regular_screen(soldier)
 
-        # # Check lose
-        # if game_field.check_lose(soldier):
-        #     # Update image to exploding
-        #     soldier.change_soldier_image(SOLDIER_EXPLODED_IMG_PATH)
-        #     screens.display_regular_screen(soldier)
-        #     pygame.display.update()
-        #     # Delay to end game
-        #     pygame.time.wait(END_GAME_DELAY)
-        #     running = False
+        # Check lose
+        if soldier.get_status() == LOSE_STATUS:
+            # Update image to exploding
+            soldier.change_soldier_image(SOLDIER_EXPLODED_IMG_PATH)
+            screens.display_regular_screen(soldier)
+            pygame.display.update()
+            # Delay to end game
+            pygame.time.wait(END_GAME_DELAY)
+            running = False
 
         pygame.display.update()
 
